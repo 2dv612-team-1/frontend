18119d72fa21b4d1c31679c31746e21b882de057
johang2012 <johan.gudmundsson2012@gmail.com>
2017-11-14 16:15:07 +0100

    fixed eslint erros

--- src/pages/CompaniesPage.jsx
+++ src/pages/CompaniesPage.jsx
@@ -3,10 +3,10 @@ import Text from '../elements/Text';
 import PageTitle from '../elements/PageTitle';
 
 const CompaniesPage = () => (
-    <div>
-        <PageTitle>Companies</PageTitle>
-        <Text>Bla bla bla</Text>
-    </div>
+  <div>
+    <PageTitle>Companies</PageTitle>
+    <Text>Bla bla bla</Text>
+  </div>
 );
 
 export default CompaniesPage;
