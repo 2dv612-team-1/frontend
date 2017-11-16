00321569c1e14ef4c5ae0dc7cc305ad1ee1f7330
johang2012 <johan.gudmundsson2012@gmail.com>
2017-11-16 23:49:38 +0100

    Add soruce maps

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
     path: path.join(paths.DIST, 'js'),
     filename: 'app.js',
   },
+  devtool: 'source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(paths.SRC, 'index.html'),
