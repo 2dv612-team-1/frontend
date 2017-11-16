badad8c964794a285dc6c9afd89b0e654d326eab
johang2012 <johan.gudmundsson2012@gmail.com>
2017-11-16 23:49:16 +0100

    Add Client lib for POST and GET request

--- src/libs/Client.js
+++ src/libs/Client.js
@@ -1,15 +1,55 @@
-class Client {
-  static POST = () => {
+import Auth from './Auth';
 
+class Client {
+  static serialize(obj) {
+    const str = [];
+    const keys = Object.keys(obj);
+    let i;
+    for (i = 0; i < keys.length; i += 1) {
+      if (Object.prototype.hasOwnProperty.call(obj, keys[i])) {
+        str.push(`${encodeURIComponent(keys[i])}=${encodeURIComponent(obj[keys[i]])}`);
+      }
+    }
+    return str.join('&');
   }
 
-  static GET = url => fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        throw Error('Request failed');
-      }
-      return response.json();
-    })
-    .then(json => json)
+  static POST(uri, obj = {}) {
+    const httpHeaders = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    };
+
+    const headers = new Headers(httpHeaders);
+    const body = this.serialize(obj);
+
+    const options = {
+      method: 'post',
+      headers,
+      body,
+    };
+
+    return fetch(uri, options)
+      .then(response => response.json()
+        .then(data => Object.assign({ status: response.status }, data)));
+  }
+
+  static GET(uri) {
+    const httpHeaders = {
+      'Content-type': 'application/x-www-form-urlencoded',
+      Authorization: `bearer ${Auth.getToken()}`,
+    };
+
+    const headers = new Headers(httpHeaders);
+
+    const options = {
+      method: 'get',
+      headers,
+    };
+
+    return fetch(uri, options)
+      .then(response => response.json()
+        .then(data => Object.assign({ status: response.status }, data)))
+      .catch((err) => { console.log(err); });
+  }
 }
+
 export default Client;
