ab5bf5f255588a851044f0b8083e5db243091f51
johang2012 <johan.gudmundsson2012@gmail.com>
2017-11-16 20:45:45 +0100

    Feedback:
    Vi har stöd för ES7 class properties, så därför behöver vi inte använde constructor :)

--- src/pages/CompaniesPage.jsx
+++ src/pages/CompaniesPage.jsx
@@ -8,29 +8,50 @@ import SubmitButton from '../components/SubmitButton';
 
 
 class CompaniesPage extends Component {
-  constructor(props) {
-    super(props);
-    this.url = 'https://jsonplaceholder.typicode.com/users'; // TODO: change to the actual url
-    this.state = { requestFailed: false };
-  }
+    state = { requestFailed: false };
 
-  componentDidMount() {
-    fetch(this.url)
-      .then((response) => {
-        if (!response.ok) {
-          throw Error('Request failed');
-        }
-        return response.json();
-      })
-      .then((json) => {
-        this.setState({ data: json });
-      }, () => {
-        this.setState({ requestFailed: true });
-      });
-  }
 
-  render() {
-    if (this.state.requestFailed) {
+    componentDidMount() {
+      const url = 'https://jsonplaceholder.typicode.com/users'; // TODO: change to the actual url
+      fetch(url)
+        .then((response) => {
+          if (!response.ok) {
+            throw Error('Request failed');
+          }
+          return response.json();
+        })
+        .then((json) => {
+          this.setState({ data: json });
+        }, () => {
+          this.setState({ requestFailed: true });
+        });
+    }
+
+    render() {
+      if (this.state.requestFailed) {
+        return (
+          <Modal>
+            <PageTitle>Companies</PageTitle>
+            <Text>Bla bla bla</Text>
+            <Link to="/register">
+              <SubmitButton>Create New Company</SubmitButton>
+            </Link>
+            <p>Failed!</p>
+          </Modal>
+        );
+      }
+      if (!this.state.data) {
+        return (
+          <Modal>
+            <PageTitle>Companies</PageTitle>
+            <Text>Bla bla bla</Text>
+            <Link to="/register">
+              <SubmitButton>Create New Company</SubmitButton>
+            </Link>
+            <p>Loading...</p>
+          </Modal>
+        );
+      }
       return (
         <Modal>
           <PageTitle>Companies</PageTitle>
@@ -38,33 +59,10 @@ class CompaniesPage extends Component {
           <Link to="/register">
             <SubmitButton>Create New Company</SubmitButton>
           </Link>
-          <p>Failed!</p>
+          <List list={this.state.data} />
         </Modal>
       );
     }
-    if (!this.state.data) {
-      return (
-        <Modal>
-          <PageTitle>Companies</PageTitle>
-          <Text>Bla bla bla</Text>
-          <Link to="/register">
-            <SubmitButton>Create New Company</SubmitButton>
-          </Link>
-          <p>Loading...</p>
-        </Modal>
-      );
-    }
-    return (
-      <Modal>
-        <PageTitle>Companies</PageTitle>
-        <Text>Bla bla bla</Text>
-        <Link to="/register">
-          <SubmitButton>Create New Company</SubmitButton>
-        </Link>
-        <List list={this.state.data} />
-      </Modal>
-    );
-  }
 }
 
 export default CompaniesPage;
