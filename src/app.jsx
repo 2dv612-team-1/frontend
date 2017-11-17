e8e71e8459fde00cb75d70c2e090357853b5548b
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-17 23:20:48 +0100

    Add polyfill

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+import 'raf/polyfill';
 import BasePage from './pages/BasePage';
 
 render(
