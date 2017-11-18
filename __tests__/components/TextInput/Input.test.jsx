4e8b7c569e0da65ee485fbe1b0882c4d1b873db8
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-18 15:10:03 +0100

    Add test for Input component

---
+++ __tests__/components/TextInput/Input.test.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Input from "../../../src/components/TextInput/Input";
+
+describe("Input", () => {
+  let props;
+  let wrapper;
+
+  const Wrapper = () => {
+    if (!wrapper) {
+      wrapper = shallow(<Input>{props.children}</Input>);
+    }
+    return wrapper;
+  };
+
+  // Reset Component and Props before each test.
+  beforeEach(() => {
+    props = {
+      children: undefined
+    };
+    wrapper = undefined;
+  });
+
+  it("should render a input element", () => {
+    const input = Wrapper().find("input");
+    expect(input.length).toBeGreaterThan(0);
+  });
+});
