cf9c302e135b617cf024b6867e5a4e8da2a696a6
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-18 11:30:05 +0100

    Add test for modal component

---
+++ __tests__/components/Modal/index.test.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Modal from "../../../src/components/Modal/";
+
+describe("Modal", () => {
+  let props;
+  let wrapper;
+
+  const Wrapper = () => {
+    if (!wrapper) {
+      wrapper = shallow(<Modal {...props} />);
+    }
+    return wrapper;
+  };
+
+  // Reset Component and Props before each test.
+  beforeEach(() => {
+    props = {
+      onChange: undefined,
+      onSubmit: undefined,
+      fields: {}
+    };
+    wrapper = undefined;
+  });
+
+  it("should render", () => {});
+    const div = Wrapper().find("div");
+    expect(div.length).toBe(1)
+});
