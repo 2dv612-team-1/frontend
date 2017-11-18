c582c1921a7376c15cd66b64e99dd9f547cc2c92
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-18 02:27:03 +0100

    Add test for List component

---
+++ __tests__/components/List/index.text.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { shallow } from "enzyme";
+import List from "../../../src/components/List/";
+
+describe("List", () => {
+  let props;
+  let wrapper;
+
+  const Wrapper = () => {
+    if (!wrapper) {
+      wrapper = shallow(<List {...props} />);
+    }
+    return wrapper;
+  };
+
+  // Reset Component and Props before each test.
+  beforeEach(() => {
+    props = {
+      list: undefined
+    };
+    wrapper = undefined;
+  });
+
+  it("should render a List component", () => {
+    const list = Wrapper().find("ol");
+    expect(list.length).toBeGreaterThan(0);
+  });
+
+  describe("when props.list is undefined", () => {
+    it("should not render list items", () => {
+      const listItems = Wrapper()
+        .find("ol")
+        .children();
+      expect(listItems.length).toBe(0);
+    });
+  });
+
+  describe("when props.list is an array with items", () => {
+    it("should render a list", () => {
+      props.list = [
+        { username: "karl" },
+        { username: "karl" },
+        { username: "karl" },
+        { username: "karl" },
+        { username: "karl" }
+      ];
+
+      const listItems = Wrapper()
+        .find("ol")
+        .children();
+      expect(listItems.length).toBe(5);
+    });
+  });
+});
