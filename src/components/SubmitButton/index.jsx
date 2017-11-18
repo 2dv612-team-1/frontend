0763dd0493120c09105bd7ab581ffdd7c7d32321
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-18 14:47:08 +0100

    Add defaultProp for props.children
    Added "submit" as default prop for props.children and removes isRequired from propTypes.

--- src/components/SubmitButton/index.jsx
+++ src/components/SubmitButton/index.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+const defaultProps = {
+  children: "submit"
+};
+
 const propTypes = {
-  children: PropTypes.string.isRequired
+  children: PropTypes.string
 };
 
 const SubmitButton = ({ children }) => <input type="submit" value={children} />;
 
+SubmitButton.defaultProps = defaultProps;
 SubmitButton.propTypes = propTypes;
 
 export default SubmitButton;
