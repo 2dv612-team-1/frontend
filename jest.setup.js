616720f883719bde5bb9de7765038ac65d88bd71
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-19 13:58:13 +0100

    Add mock support for localStorage

--- jest.setup.js
+++ jest.setup.js
@@ -2,5 +2,6 @@ import "raf/polyfill";
 
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import "jest-localstorage-mock";
 
 Enzyme.configure({ adapter: new Adapter() });
