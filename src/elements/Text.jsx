6dcfdb4024ca4f573b88fe182518be6858362690
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-20 18:24:10 +0100

    Merge Text components

--- src/elements/Text.jsx
+++ src/elements/Text.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Text = styled.p`
   font-size: 16px;
-  color: black;
+  color: ${props => (props.error ? "red" : "black")};
   text-align: ${props => (props.center ? "center" : "left")};
 `;
 
