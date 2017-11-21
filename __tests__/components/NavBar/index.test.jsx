c0621789e48deb5019a8efb7611018fa50113a66
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-21 22:35:27 +0100

    Remove fragile tests

--- __tests__/components/NavBar/index.test.jsx
+++ __tests__/components/NavBar/index.test.jsx
@@ -2,10 +2,8 @@ import React from "react";
 import { shallow } from "enzyme";
 import NavBar from "../../../src/components/NavBar/";
 import Container from "../../../src/components/NavBar/Container";
-import MenuItem from "../../../src/components/NavBar/MenuItem";
 
 describe("NavBar", () => {
-  let props;
   let wrapper;
 
   const Wrapper = () => {
@@ -24,60 +22,5 @@ describe("NavBar", () => {
   it("should render", () => {
     const container = Wrapper().find(Container);
     expect(container.length).toBeGreaterThan(0);
-  });
-
-  it("should have link to home", () => {
-    const text = "home";
-    const route = "/";
-    const link = Wrapper()
-      .find(MenuItem)
-      .at(0);
-
-    expect(link.props().children).toBe(text)
-    expect(link.props().to).toBe(route)
-  });
-
-  it("should have link to login", () => {
-    const text = "login";
-    const route = "/login";
-    const link = Wrapper()
-      .find(MenuItem)
-      .at(1);
-
-    expect(link.props().children).toBe(text)
-    expect(link.props().to).toBe(route)
-  });
-
-  it("should have link to register", () => {
-    const text = "register";
-    const route = "/register/admin";
-    const link = Wrapper()
-      .find(MenuItem)
-      .at(2);
-
-    expect(link.props().children).toBe(text)
-    expect(link.props().to).toBe(route)
-  });
-
-  it("should have link to companies", () => {
-    const text = "companies";
-    const route = "/companies";
-    const link = Wrapper()
-      .find(MenuItem)
-      .at(3);
-
-    expect(link.props().children).toBe(text)
-    expect(link.props().to).toBe(route)
-  });
-
-  it("should have link to representatives", () => {
-    const text = "representatives";
-    const route = "/representatives";
-    const link = Wrapper()
-      .find(MenuItem)
-      .at(4);
-
-    expect(link.props().children).toBe(text)
-    expect(link.props().to).toBe(route)
   });
 });
