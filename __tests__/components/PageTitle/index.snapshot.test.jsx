02b3c015136e38096244a269f12d81187a5c6e97
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-21 22:56:39 +0100

    Move and Rename test

--- __tests__/components/PageTitle/index.snapshot.test.jsx
+++ __tests__/components/PageTitle/index.snapshot.test.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 import "jest-styled-components";
-import PageTitle from "../../src/elements/PageTitle";
-import colors from "../../src/constants/colors";
+import PageTitle from "../../../src/components/PageTitle";
+import colors from "../../../src/constants/colors";
 
 describe("PageTitle", () => {
   let props;
