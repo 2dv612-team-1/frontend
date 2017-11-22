59bddbdff7b2e856731b48a9ea8a5aa4b973e039
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-22 20:27:37 +0100

    Fix prettier issues

--- src/elements/Input.jsx
+++ src/elements/Input.jsx
@@ -2,16 +2,16 @@ import styled from "styled-components";
 import fonts from "../constants/fonts";
 
 const Input = styled.input`
-	display: block;
-	margin: 0 auto;
-	width: 300px;
-	height: 40px;
-	text-align: center;
-	font-family: ${fonts.secondary};
-	letter-spacing: 0.1em;
-	font-size: 1em;
-	margin-top: 5px;
-	margin-bottom: 20px;
+  display: block;
+  margin: 0 auto;
+  width: 300px;
+  height: 40px;
+  text-align: center;
+  font-family: ${fonts.secondary};
+  letter-spacing: 0.1em;
+  font-size: 1em;
+  margin-top: 5px;
+  margin-bottom: 20px;
 `;
 
 export default Input;
