ff6cacf8bd3ff26de687679a58e85afbc880866c
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-22 20:42:49 +0100

    Fix prettier issues

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -3,11 +3,11 @@ import colors from "../../constants/colors";
 import fonts from "../../constants/fonts";
 
 const OList = styled.ol`
-	font-size: 16px;
-	text-transform: capitalize;
-	font-family: ${fonts.secondary};
-	list-style-position: inside;
-	padding: 0;
+  font-size: 16px;
+  text-transform: capitalize;
+  font-family: ${fonts.secondary};
+  list-style-position: inside;
+  padding: 0;
 `;
 
 export default OList;
