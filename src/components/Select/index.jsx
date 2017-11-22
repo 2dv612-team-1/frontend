cff21dc571f3b2edb14cc888fecfe964ba731b28
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-22 20:33:24 +0100

    Fix prettier issues

--- src/components/Select/index.jsx
+++ src/components/Select/index.jsx
@@ -3,16 +3,16 @@ import PropTypes from "prop-types";
 import Select from "./Select";
 
 const propTypes = {
-	onChange: PropTypes.func.isRequired
+  onChange: PropTypes.func.isRequired
 };
 
 const DropDown = ({ onChange }) => (
-	<Select onChange={onChange} name="role">
-		<option value="admin">admin</option>
-		<option value="representative">representative</option>
-		<option value="company">company</option>
-		<option value="customer">customer</option>
-	</Select>
+  <Select onChange={onChange} name="role">
+    <option value="admin">admin</option>
+    <option value="representative">representative</option>
+    <option value="company">company</option>
+    <option value="customer">customer</option>
+  </Select>
 );
 
 DropDown.propTypes = propTypes;
