668c1620825eec42bf1d055b1494a0ac89582ab9
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-22 20:43:06 +0100

    Fix prettier issues

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -3,21 +3,21 @@ import PropTypes from "prop-types";
 import OList from "./List";
 
 const defaultProps = {
-	list: []
+  list: []
 };
 
 const propTypes = {
-	list: PropTypes.arrayOf(
-		PropTypes.shape({
-			usarname: PropTypes.string
-		})
-	)
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      usarname: PropTypes.string
+    })
+  )
 };
 
 const List = ({ list }) => (
-	<OList>
-		{list.map(item => <li key={item.username}>{item.username}</li>)}
-	</OList>
+  <OList>
+    {list.map(item => <li key={item.username}>{item.username}</li>)}
+  </OList>
 );
 
 List.defaultProps = defaultProps;
