e8dfa3a19d731f14c382f923da208b3d15890660
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-22 20:32:55 +0100

    Fix prettier issues

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -2,15 +2,15 @@ import styled from "styled-components";
 import fonts from "../../constants/fonts";
 
 const Select = styled.select`
-	display: block;
-	width: 300px;
-	height: 40px;
-	margin: 0 auto;
-	font-size: 1em;
-	text-transform: capitalize;
-	margin-bottom: 20px;
-	margin-top: 20px;
-	text-align: center;
+  display: block;
+  width: 300px;
+  height: 40px;
+  margin: 0 auto;
+  font-size: 1em;
+  text-transform: capitalize;
+  margin-bottom: 20px;
+  margin-top: 20px;
+  text-align: center;
 `;
 
 export default Select;
