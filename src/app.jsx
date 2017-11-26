d5d743eba13dd5438c9d4e472b599fb23c909e19
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-26 15:30:14 +0100

    Add Redux

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 import "core-js/es6/map";
 import "core-js/es6/set";
 import "raf/polyfill";
 import BasePage from "./pages/BasePage";
+import configureStore from "./store/configureStore";
+
+const store = configureStore();
 
 render(
-  <Router>
-    <BasePage />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <BasePage />
+    </Router>
+  </Provider>,
   document.getElementById("app")
 );
