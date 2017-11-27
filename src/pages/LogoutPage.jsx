f60215c13319058b2e163d40ca8c34840124c8d9
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-27 22:25:32 +0100

    Remove unused import

--- src/pages/LogoutPage.jsx
+++ src/pages/LogoutPage.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
-import Auth from "../libs/Auth";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { performLogout } from "../actions/session";
