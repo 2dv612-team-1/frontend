a92c9a1a151647e3cb594763782f62da213fdd02
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-27 22:25:19 +0100

    Add propTypes

--- src/pages/LogoutPage.jsx
+++ src/pages/LogoutPage.jsx
@@ -2,12 +2,16 @@ import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import Auth from "../libs/Auth";
 import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import { performLogout } from "../actions/session";
 
 class LogoutPage extends Component {
   state = {
     redirect: false
   };
+const propTypes = {
+  performLogout: PropTypes.func.isRequired
+};
 
   componentWillMount() {
     this.props.performLogout();
@@ -21,4 +25,6 @@ class LogoutPage extends Component {
 const mapDispatchToProps = dispatch => ({
   performLogout: () => dispatch(performLogout())
 });
+
+LogoutPage.propTypes = propTypes;
 export default connect(null, mapDispatchToProps)(LogoutPage);
