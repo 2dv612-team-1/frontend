d3b3fe9db9630038af39a32ea4ea7f8d240ab242
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-27 21:09:16 +0100

    Replace react state with redux state

--- src/pages/BasePage.jsx
+++ src/pages/BasePage.jsx
@@ -1,22 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import Routes from "../routes/";
 import NavBar from "../components/NavBar";
-import Auth from "../libs/Auth";
-import Jwt from "../libs/Jwt";
 
-class BasePage extends Component {
-  state = {
-    role: Auth.isUserAuthenticated() ? Jwt.getRole(Auth.getToken()) : ""
-  };
-
-  render() {
-    return (
-      <div>
-        <NavBar role={this.state.role} />
-        <Routes role={this.state.role} />
-      </div>
-    );
-  }
-}
+const BasePage = () => (
+  <div>
+    <NavBar />
+    <Routes />
+  </div>
+);
 
 export default BasePage;
