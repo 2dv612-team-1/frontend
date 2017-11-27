de7675df46c7b72b184fc75ac090dbd4efc7e684
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-27 21:04:13 +0100

    Add default state

--- src/reducers/session.js
+++ src/reducers/session.js
@@ -18,7 +18,7 @@ export function loginHasError(state = false, action) {
   }
 }
 
-export function isLogedIn(state = {}, action) {
+export function logedInAs(state = { isLoggedIn: false, role: "" }, action) {
   switch (action.type) {
     case "LOGEDIN_AS":
       return Object.assign({}, state, {
