a7a73b80b57e5554ff2ab5499e2527271f22a2f0
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-27 23:32:39 +0100

    Change to re-ducks structrue pattern

---
+++ src/state/companies/actions.js
@@ -0,0 +1,36 @@
+import types from "./types";
+import Client from "../../libs/Client";
+
+export const companiesHasError = bool => ({
+  type: types.COMPANIES_HAS_ERROR,
+  hasError: bool
+});
+
+export const companiesIsLoading = bool => ({
+  type: types.COMPANIES_IS_LOADING,
+  isLoading: bool
+});
+
+export const companiesFetchDataSucces = companies => ({
+  type: types.COMPANIES_FETCH_DATA_SUCCESS,
+  companies
+});
+
+export const companiesFetchData = url => dispatch => {
+  dispatch(companiesIsLoading(true));
+  Client.GET(url)
+    .then(data => {
+      dispatch(companiesFetchDataSucces(data.companies));
+    })
+    .catch(() => {
+      dispatch(companiesIsLoading(false));
+      dispatch(companiesHasError(true));
+    });
+};
+
+export default {
+  companiesHasError,
+  companiesIsLoading,
+  companiesFetchDataSucces,
+  companiesFetchData
+};
