e508a964b0a6335ce9f7418b3fd4811465147550
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-27 20:58:50 +0100

    Remove console.log

--- src/libs/Jwt.js
+++ src/libs/Jwt.js
@@ -3,7 +3,6 @@ import JwtDecode from "jwt-decode";
 class Jwt {
   static decode(token) {
     const decoded = JwtDecode(token);
-    console.log(decoded);
     return decoded;
   }
   static getUsername(token) {
