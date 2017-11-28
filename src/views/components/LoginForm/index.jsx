5adff73c78e0c079270ccc892b809804a3f8dfaa
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-28 23:48:49 +0100

    Change to redux-form

--- src/views/components/LoginForm/index.jsx
+++ src/views/components/LoginForm/index.jsx
@@ -1,36 +1,44 @@
 import React from "react";
 import PropTypes from "prop-types";
-import TextInput from "../TextInput";
+import { reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { performLogin } from "../../../state/session/actions";
 import Button from "../Button";
 import Form from "../../elements/Form";
+import Field from "../../components/Field";
 
 const propTypes = {
-  onChange: PropTypes.func.isRequired,
-  onSubmit: PropTypes.func.isRequired,
-  fields: PropTypes.shape({
-    username: "",
-    password: ""
-  }).isRequired
+  handleSubmit: PropTypes.func.isRequired,
+  login: PropTypes.func.isRequired,
+  admin: PropTypes.string.isRequired
 };
 
-const LoginForm = ({ onChange, onSubmit, fields }) => (
-  <Form onSubmit={onSubmit}>
-    <TextInput
-      label="username"
-      onChange={onChange}
-      name="username"
-      value={fields.username}
-    />
-    <TextInput
-      label="password"
-      onChange={onChange}
-      name="password"
-      value={fields.password}
-    />
-    <Button form>Login</Button>
-  </Form>
-);
+let LoginForm = ({ handleSubmit, login, admin }) => {
+  const onSubmit = values => {
+    let url = "https://nanotu.be/auth";
+    if (admin === "true") {
+      url = "https://nanotu.be/admins/auth";
+    }
+    login(url, values);
+  };
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <Field label="username" name="username" component="input" type="text" />
+      <Field label="password" name="password" component="input" type="text" />
+      <Button form>Login</Button>
+    </Form>
+  );
+};
 
 LoginForm.propTypes = propTypes;
 
-export default LoginForm;
+LoginForm = reduxForm({
+  form: "login"
+})(LoginForm);
+
+const mapDispatchToProps = dispatch => ({
+  login: (url, obj) => dispatch(performLogin(url, obj))
+});
+
+export default connect(null, mapDispatchToProps)(LoginForm);
