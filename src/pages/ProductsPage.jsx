73c59aafc614d6d5de890d725b55ff4c6b872940
isabelberg <33488944+isabelberg@users.noreply.github.com>
2017-11-28 15:40:42 +0000

    productspage

---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Text from "../elements/Text";
+import Modal from "../components/Modal";
+import PageTitle from "../components/PageTitle";
+import List from "../components/List";
+import ErrorMessage from "../components/ErrorMessage";
+import Client from "../libs/Client";
+import Button from "../components/Button";
+
+class ProductsPage extends Component {
+  state = {
+    error: "",
+    data: []
+  };
+
+  componentDidMount() {
+    const url = "https://nanotu.be/products";
+    Client.GET(url)
+      .then(data => {
+        this.setState({ data: data.products });
+      })
+      .catch(() => {
+        this.setState({ error: "Could not load data" });
+      });
+  }
+
+  render() {
+    return (
+      <Modal>
+        <PageTitle>Products</PageTitle>
+        <Link to="/register/product">
+          <Button>Add Product</Button>
+        </Link>
+        <Text>All products:</Text>
+        {this.state.data ? (
+          <List list={this.state.data} />
+        ) : (
+          <Text>Loading...</Text>
+        )}
+        <ErrorMessage>{this.state.error}</ErrorMessage>
+      </Modal>
+    );
+  }
+}
+
+export default CompaniesPage;
