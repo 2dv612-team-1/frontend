74ac87295b464c5d82ea0f2c99f3daf1e62a8d63
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-28 23:47:07 +0100

    Add react-router-redux

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
+import createHistory from "history/createBrowserHistory";
+import { ConnectedRouter } from "react-router-redux";
 import "core-js/es6/map";
 import "core-js/es6/set";
 import "raf/polyfill";
@@ -9,12 +11,13 @@ import BasePage from "./views/pages/BasePage";
 import configureStore from "./state/store";
 
 const store = configureStore();
+const history = createHistory();
 
 render(
   <Provider store={store}>
-    <Router>
+    <ConnectedRouter history={history}>
       <BasePage />
-    </Router>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById("app")
 );
