e3785d6e76fcd07e5e5dca0ddc8dd2677143fe86
isabelberg <33488944+isabelberg@users.noreply.github.com>
2017-11-30 11:46:11 +0000

    redux for products

---
+++ src/state/products/index.js
@@ -0,0 +1,7 @@
+import productsReducer from "./reducers";
+
+//  export { default as companiesSelectors } from "./selectors";
+//  export { default as companiesOperations } from "./operations";
+export { default as productsTypes } from "./types";
+
+export default productsReducer;
