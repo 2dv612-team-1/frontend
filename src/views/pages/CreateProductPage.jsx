faf4577638ad3c90de66f3317b0efa4bce7048bf
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-30 19:52:53 +0100

    Add demo contnet

--- src/views/pages/CreateProductPage.jsx
+++ src/views/pages/CreateProductPage.jsx
@@ -1,11 +1,37 @@
 import React, { Component } from "react";
+import { reduxForm } from 'redux-form';
+import Field from "../components/Field";
+import Button from "../components/Button";
+import PageContainer from "../components/PageContainer";
 
 class CreateProductPage extends Component {
   render() {
     return (
-      <p>hello world</p>
+      <PageContainer title="create product">
+        <form>
+          <Field
+            label="category"
+            name="category"
+            component="input"
+            type="text"
+          />
+          <Field label="title" name="title" component="input" type="text" />
+          <Field
+            label="description"
+            name="description"
+            component="input"
+            type="text"
+          />
+          <Button>Upload file</Button>
+          <Button submit>create</Button>
+        </form>
+      </PageContainer>
     );
   }
 }
+
+CreateProductPage = reduxForm({
+  form: "createProduct"
+})(CreateProductPage);
 
 export default CreateProductPage;
