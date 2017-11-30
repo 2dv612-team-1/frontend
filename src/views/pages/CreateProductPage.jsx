06304e0c9d4b3f2b245765a2bc3231fc1a70c044
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-11-30 23:04:57 +0100

    Connect for to redux-form

--- src/views/pages/CreateProductPage.jsx
+++ src/views/pages/CreateProductPage.jsx
@@ -1,35 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 import { reduxForm } from "redux-form";
 import Field from "../components/Field";
 import SelectField from "../components/SelectField";
 import Button from "../components/Button";
 import PageContainer from "../components/PageContainer";
+import DropZoneField from "../components/DropZoneField";
 
-class CreateProductPage extends Component {
-  render() {
-    return (
-      <PageContainer title="create product">
-        <form>
-          <SelectField
-            label="category"
-            name="category"
-            type="text"
-            options={["test1", "test2", "test3"]}
-          />
-          <Field label="title" name="title" component="input" type="text" />
-          <Field
-            label="description"
-            name="description"
-            component="input"
-            type="text"
-          />
-          <Button>Upload file</Button>
-          <Button submit>create</Button>
-        </form>
-      </PageContainer>
-    );
-  }
-}
+let CreateProductPage = () => (
+  <PageContainer title="create product">
+    <form>
+      <SelectField
+        label="category"
+        name="category"
+        type="text"
+        options={["test1", "test2", "test3"]}
+      />
+      <Field label="title" name="title" component="input" type="text" />
+      <Field
+        label="description"
+        name="description"
+        component="input"
+        type="text"
+      />
+      <Field name="files" component={DropZoneField} />
+      <Button submit>create</Button>
+    </form>
+  </PageContainer>
+);
 
 CreateProductPage = reduxForm({
   form: "createProduct"
