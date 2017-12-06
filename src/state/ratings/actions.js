225b313f9c77dda0f1720631b3d6ca64dd5f0e41
isabelberg <33488944+isabelberg@users.noreply.github.com>
2017-12-06 14:10:21 +0000

    changes in rating actions

--- src/state/ratings/actions.js
+++ src/state/ratings/actions.js
@@ -3,22 +3,22 @@ import Client from "../../libs/Client";
 import Auth from "../../libs/Auth";
 
 export const ratingsHasError = bool => ({
-  type: types.ratingS_HAS_ERROR,
+  type: types.RATINGS_HAS_ERROR,
   hasError: bool
 });
 
 export const ratingsIsLoading = bool => ({
-  type: types.ratingS_IS_LOADING,
+  type: types.RATINGS_IS_LOADING,
   isLoading: bool
 });
 
 export const ratingsFetchDataSuccess = ratings => ({
-  type: types.ratingS_FETCH_DATA_SUCCESS,
+  type: types.RATINGS_FETCH_DATA_SUCCESS,
   ratings
 });
 
 export const ratingsPostDataSuccess = (bool, message) => ({
-  type: types.ratingS_POST_DATA_SUCCESS,
+  type: types.RATINGS_POST_DATA_SUCCESS,
   isSuccess: bool,
   successMessage: message
 });
@@ -37,29 +37,10 @@ export const ratingsFetchData = url => dispatch => {
     });
 };
 
-export const uploadCreatedrating = (url, obj) => dispatch => {
-  const toUpload = Object.assign({}, obj);
-  toUpload.jwt = Auth.getToken();
-
-  const body = new FormData();
-  Object.keys(toUpload).forEach(key => {
-    body.append(key, toUpload[key]);
-  });
-
-  console.info("POST", body, toUpload);
-  console.info("This is expected to fail:");
-  fetch(url, {
-    method: "POST",
-    body: body
-  })
-    .then(res => res.json())
-    .then(res => console.log(res))
-    .catch(err => console.error(err));
-  /*  Client.POST(url, toUpload)
-    //  .then(data => {})
-    .catch(() => {
-      //  dispatch(ratingUploadHasError(true));
-    }); */
+export const ratingsPostData = (url, obj) => dispatch => {
+  const rating = Object.assign({}, obj);
+  rating.jwt = Auth.getToken();
+  Client.POST(url).then(data => {});
 };
 
 export default {
