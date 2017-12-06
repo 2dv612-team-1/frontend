596c4b90e45b4ab77acf327cbe0db335fa8f0c62
isabelberg <33488944+isabelberg@users.noreply.github.com>
2017-12-06 14:43:55 +0000

    added actions

--- src/state/ratings/actions.js
+++ src/state/ratings/actions.js
@@ -2,51 +2,32 @@ import types from "./types";
 import Client from "../../libs/Client";
 import Auth from "../../libs/Auth";
 
-export const ratingsHasError = bool => ({
-  type: types.RATINGS_HAS_ERROR,
-  hasError: bool
+export const ratingHasError = products => ({
+  type: types.RATING_HAS_ERROR,
+  rating
 });
 
-export const ratingsIsLoading = bool => ({
-  type: types.RATINGS_IS_LOADING,
-  isLoading: bool
+export const ratingFetchDataSuccess = products => ({
+  type: types.RATING_FETCH_DATA_SUCCESS,
+  rating
 });
 
-export const ratingsFetchDataSuccess = ratings => ({
-  type: types.RATINGS_FETCH_DATA_SUCCESS,
-  ratings
-});
-
-export const ratingsPostDataSuccess = (bool, message) => ({
-  type: types.RATINGS_POST_DATA_SUCCESS,
+export const ratingPostDataSuccess = (bool, message) => ({
+  type: types.RATING_POST_DATA_SUCCESS,
   isSuccess: bool,
   successMessage: message
 });
 
-export const ratingsFetchData = url => dispatch => {
-  dispatch(ratingsIsLoading(true));
-  Client.GET(url)
-    .then(data => {
-      dispatch(ratingsFetchDataSuccess(data.data.ratings));
-      dispatch(ratingsIsLoading(false));
-    })
-    .catch(err => {
-      console.log(err);
-      dispatch(ratingsIsLoading(false));
-      dispatch(ratingsHasError(true));
-    });
-};
-
-export const ratingsPostData = (url, obj) => dispatch => {
-  const rating = Object.assign({}, obj);
-  rating.jwt = Auth.getToken();
-  Client.POST(url).then(data => {});
+export const ratingPostRate = url => dispatch => {
+  dispatch(ratingFetchDataSuccess(true));
+  Client.POST(url, obj).then(data => {
+    dispatch(productsFetchDataSuccess(data.data.rating));
+  });
 };
 
 export default {
-  ratingsHasError,
-  ratingsIsLoading,
-  ratingsFetchDataSuccess,
-  ratingsPostDataSuccess,
-  ratingsFetchData
+  ratingHasError,
+  ratingFetchDataSuccess,
+  ratingPostDataSuccess,
+  ratingPostRate
 };
