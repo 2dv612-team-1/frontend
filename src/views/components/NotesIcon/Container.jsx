a17887e8348e35420dead3343de7a60a06289545
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-12-07 00:21:56 +0100

    Update event handlers

--- src/views/components/NotesIcon/Container.jsx
+++ src/views/components/NotesIcon/Container.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const Container = styled.div`
   float: right;
+  cursor: pointer;
 `;
 
 export default Container;
--- src/views/components/NotesIcon/index.jsx
+++ src/views/components/NotesIcon/index.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import Container from "./Container";
 
 const NotesIcon = ({ id, onClick }) => (
-  <Container name={id} onClick={onClick}>
-    <i className="fa fa-sticky-note-o" aria-hidden="true" />
+  <Container>
+    <i
+      name={id}
+      onClick={onClick}
+      className="fa fa-sticky-note-o"
+      aria-hidden="true"
+    />
   </Container>
 );
 
