73e6d26f64df940d463114892f9d56a0cbe5d4fa
isabelberg <33488944+isabelberg@users.noreply.github.com>
2017-12-07 14:42:17 +0000

    styling for rating objects

---
+++ src/views/components/Rating/Rate.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Rate = styled.span`
+	width: 24px;
+	height: 24px;
+	float: left;
+	margin-left: 10px;
+	background-image: url("https://i.imgur.com/XEkHaRJ.png");
+	background-repeat: no-repeat;
+	background-position: center center;
+	background-size: contain;
+`;
+
+export default Rate;
