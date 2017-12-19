49f3859943c3d16bfbc71e45b45bb30902e77e45
Olga Christensen <oc222ba@student.lnu.se>
2017-12-19 13:21:55 +0100

    Adding state for Forum

---
+++ src/state/forum/actions.js
@@ -0,0 +1,75 @@
+import types from "./types";
+import Client from "../../libs/Client";
+
+export const forumHasError = (bool, err) => ({
+  type: types.FORUM_HAS_ERROR,
+  hasError: bool,
+  errorMessage: err
+});
+
+export const forumIsLoading = bool => ({
+  type: types.FORUM_IS_LOADING,
+  isLoading: bool
+});
+
+export const forumFetchDataSuccess = forum => ({
+  type: types.FORUM_FETCH_DATA_SUCCESS,
+  forum
+});
+
+export const forumPostDataSuccess = (bool, message) => ({
+  type: types.FORUM_POST_DATA_SUCCESS,
+  isSuccess: bool,
+  successMessage: message
+});
+
+export const forumClear = () => dispatch => {
+  dispatch(forumIsLoading(false));
+  dispatch(forumHasError(false, ""));
+  dispatch(forumPostDataSuccess(false, ""));
+};
+
+export const forumFetchData = url => dispatch => {
+  dispatch(forumIsLoading(true));
+  Client.GET(url)
+    .then(data => {
+      dispatch(forumFetchDataSuccess(data.message));
+      dispatch(forumIsLoading(false));
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch(forumIsLoading(false));
+      dispatch(forumHasError(true, err));
+    });
+};
+
+export const forumPostData = (url, fields) => dispatch => {
+  dispatch(forumIsLoading(true));
+  dispatch(forumClear());
+  Client.POST(url, fields)
+    .then(data => {
+      console.log(data);
+      if (data.status !== 201) {
+        dispatch(forumHasError(true, data.message));
+        dispatch(forumIsLoading(false));
+      } else {
+        dispatch(forumPostDataSuccess(true, data.message));
+        dispatch(forumHasError(false));
+        dispatch(forumIsLoading(false));
+      }
+    })
+    .catch(err => {
+      dispatch(forumHasError(true, err.message));
+      dispatch(forumIsLoading(false));
+    });
+};
+
+export default {
+  forumHasError,
+  forumIsLoading,
+  forumFetchDataSuccess,
+  forumPostDataSuccess,
+  forumFetchData,
+  forumPostData,
+  forumClear
+};
---
+++ src/state/forum/reducers.js
@@ -0,0 +1,57 @@
+import { combineReducers } from "redux";
+//  import CONSTANTS from "./constants";
+import types from "./types";
+
+const forumHasError = (state = false, action) => {
+  switch (action.type) {
+    case types.FORUM_HAS_ERROR:
+      return {
+        hasError: action.hasError,
+        errorMessage: action.errorMessage
+      };
+
+    default:
+      return state;
+  }
+};
+
+const forumIsLoading = (state = false, action) => {
+  switch (action.type) {
+    case types.FORUM_IS_LOADING:
+      return action.isLoading;
+
+    default:
+      return state;
+  }
+};
+
+const forum = (state = [], action) => {
+  switch (action.type) {
+    case types.FORUM_FETCH_DATA_SUCCESS:
+      return action.forum;
+
+    default:
+      return state;
+  }
+};
+
+const forumPostDataSuccess = (state = false, action) => {
+  switch (action.type) {
+    case types.FORUM_POST_DATA_SUCCESS:
+      return {
+        isSuccess: action.isSuccess,
+        successMessage: action.successMessage
+      };
+    default:
+      return state;
+  }
+};
+
+const forumReducer = combineReducers({
+  forumHasError,
+  forumIsLoading,
+  forum,
+  forumPostDataSuccess
+});
+
+export default forumReducer;
