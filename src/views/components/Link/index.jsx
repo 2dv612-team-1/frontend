8797966d229ae3aa1dc96297fafc841e9ec672a0
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-12-19 19:48:11 +0100

    Fix Prettier issues

--- src/views/components/Link/index.jsx
+++ src/views/components/Link/index.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import Anchor from "./Anchor";
 
-
 const defaultProps = {
   href: "#",
   name: "Link"
-}
+};
 
 const propTypes = {
   href: Proptypes.string,
   name: PropTypes.string
-}
+};
 
 const Link = ({ href, name, newWindow }) => (
   <Anchor target={newWindow ? "_blank" : "_self"} href={href}>
