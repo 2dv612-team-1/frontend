0a3a227c7b698b0e68a6333889c8f773ca461263
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-12-19 19:44:53 +0100

    Fixed prettier issues

--- src/views/components/Link/index.jsx
+++ src/views/components/Link/index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Anchor from "./Anchor";
 
-const Link = ({ href, name, newWindow }) => <Anchor target={newWindow ? "_blank" : "_self" }  href={href}>{name}</Anchor>;
+const Link = ({ href, name, newWindow }) => (
+  <Anchor target={newWindow ? "_blank" : "_self"} href={href}>
+    {name}
+  </Anchor>
+);
 
 export default Link;
