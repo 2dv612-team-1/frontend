4f779365f3bfe0cdd3567336cdbf7b8ec19c4e1b
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-12-19 21:01:02 +0100

    Fix prettier issues

--- src/views/components/CategoriesList/index.jsx
+++ src/views/components/CategoriesList/index.jsx
@@ -7,12 +7,10 @@ import SubList from "./SubList";
 const CategoriesList = ({ items }) => {
   <div>
     {items.map((item, index) => {
-        <div key={i}>
-          <MyOl>
-            {`${i + 1 }. ${item.category}`}
-          </MyOl>
-          {item.sub !== null ? <SubList parent={i} items={item.sub} /> : null}
-        </div>
+      <div key={i}>
+        <MyOl>{`${i + 1}. ${item.category}`}</MyOl>
+        {item.sub !== null ? <SubList parent={i} items={item.sub} /> : null}
+      </div>;
     })}
   </div>;
 };
