c504d82289528cb04d531436da625ea49c215f7f
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-12-19 21:21:38 +0100

    Move UnorderedList to Elements

--- src/views/components/List/index.jsx
+++ src/views/components/List/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import UnorderedList from "./UnorderedList";
+import UnorderedList from "../../elements/UnorderedList";
 import OrderedList from "../../elements/OrderedList";
 
 const defaultProps = {
