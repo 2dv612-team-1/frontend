a11b96c3a10b81df1f3649da931346bb91c3351a
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-12-19 20:08:20 +0100

    Fix Prettier Issues

--- src/views/components/CategoriesList/index.jsx
+++ src/views/components/CategoriesList/index.jsx
@@ -4,27 +4,35 @@ import MyUl from "./ul";
 import MyOl from "./ol";
 
 const CategoriesList = ({ items }) => (
-    <div className="catslist">
-      {items.map((item, index) => {
-        let i = index + 1;
-        const space = ". ";
-        return (
-          <div key={i}>
-            <MyOl>{i}{space}{item.category}</MyOl>
-            {item.sub !== null
-              ? item.sub.map((subitem, n) => {
-                  let subi = `${i}.${n + 1}`;
-                  return (
-                    <MyUl key={subitem.category}>
-                      <li>{subi}{space}{subitem.category}</li>
-                    </MyUl>
-                  );
-                })
-              : null}
-          </div>
-        );
-      })}
-    </div>
+  <div className="catslist">
+    {items.map((item, index) => {
+      let i = index + 1;
+      const space = ". ";
+      return (
+        <div key={i}>
+          <MyOl>
+            {i}
+            {space}
+            {item.category}
+          </MyOl>
+          {item.sub !== null
+            ? item.sub.map((subitem, n) => {
+                let subi = `${i}.${n + 1}`;
+                return (
+                  <MyUl key={subitem.category}>
+                    <li>
+                      {subi}
+                      {space}
+                      {subitem.category}
+                    </li>
+                  </MyUl>
+                );
+              })
+            : null}
+        </div>
+      );
+    })}
+  </div>
 );
 
 export default CategoriesList;
