a24abb0b3e3dc7f0734441417f897c27649ec799
isabelberg <33488944+isabelberg@users.noreply.github.com>
2017-12-21 09:42:43 +0000

    Created UploadContainer comp

---
+++ src/views/components/UploadContainer/index.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import fonts from "../../constants/fonts";
+import colors from "../../constants/colors";
+
+const UploadContainer = styled.div`
+  clear:left;
+  float:left;
+  width:100%;
+  padding-top:10px;
+  margin-top:20px;
+  margin-bottom: 20px;
+  border-top: solid 1px rgba(0,0,0,.1);
+`;
+
+export default UploadContainer;
