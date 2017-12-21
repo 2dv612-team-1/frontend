6517e045c88d630896831e26157e51c063dbb87f
isabelberg <33488944+isabelberg@users.noreply.github.com>
2017-12-21 09:41:50 +0000

    Changed style

--- src/views/components/FileLink/index.jsx
+++ src/views/components/FileLink/index.jsx
@@ -2,9 +2,8 @@ import styled from "styled-components";
 import fonts from "../../constants/fonts";
 import colors from "../../constants/colors";
 
-const FileLink = styled.p`
+const FileLink = styled.a`
   font-size: 16px;
-  text-align: ${props => (props.center ? "center" : "left")};
   font-family: ${fonts.primary};
   margin-top: 0px;
   font-weight: 800;
