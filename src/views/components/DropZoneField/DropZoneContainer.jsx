daa026b0ebf73840aaca7dbf949f64102d34ae76
isabelberg <33488944+isabelberg@users.noreply.github.com>
2017-12-21 22:58:25 +0000

    created component DropZoneContainer

---
+++ src/views/components/DropZoneField/DropZoneContainer.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import fonts from "../../constants/fonts";
+
+const DropZoneContainer = styled.div`
+  font-family: ${fonts.primary};
+  font-size: 12pt;
+  background: none;
+  display:inline-block;
+  text-align: left;
+  cursor: pointer;
+`;
+
+export default DropZoneContainer;
