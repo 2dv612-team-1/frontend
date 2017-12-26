95553efc5fd4feac698eb9ad15c22883530a7e29
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-12-26 19:59:28 +0100

    Fix prettier issue

--- src/views/components/Field/FieldWrapper.jsx
+++ src/views/components/Field/FieldWrapper.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const FieldWrapper = styled.div`
   background: none;
   text-align: center;
-  margin-top:5px;
+  margin-top: 5px;
 `;
 
 export default FieldWrapper;
