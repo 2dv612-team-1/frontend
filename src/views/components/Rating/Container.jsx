150090a458cb98c98889b5f3e851636f3fa7b872
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-12-26 20:21:39 +0100

    Fix prettier issues

--- src/views/components/Rating/Container.jsx
+++ src/views/components/Rating/Container.jsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-	cursor: pointer;
-	float:left;
-	margin-left: 250px;
+  cursor: pointer;
+  float: left;
+  margin-left: 250px;
 `;
 
 export default Container;
