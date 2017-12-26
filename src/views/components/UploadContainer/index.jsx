129f2114ddf9f1ddf8d0d1560dbf13bb4e2c3131
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-12-26 20:38:03 +0100

    Fix prettier issues

--- src/views/components/UploadContainer/index.jsx
+++ src/views/components/UploadContainer/index.jsx
@@ -3,13 +3,13 @@ import fonts from "../../constants/fonts";
 import colors from "../../constants/colors";
 
 const UploadContainer = styled.div`
-  clear:left;
-  float:left;
-  width:100%;
-  padding-top:10px;
-  margin-top:20px;
+  clear: left;
+  float: left;
+  width: 100%;
+  padding-top: 10px;
+  margin-top: 20px;
   margin-bottom: 20px;
-  border-top: solid 1px rgba(0,0,0,.1);
+  border-top: solid 1px rgba(0, 0, 0, 0.1);
 `;
 
 export default UploadContainer;
