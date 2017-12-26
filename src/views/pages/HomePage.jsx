b74ac31cc517498f933908432e5a9488e8e32366
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-12-26 21:30:45 +0100

    Disabel eslint for line

--- src/views/pages/HomePage.jsx
+++ src/views/pages/HomePage.jsx
@@ -51,7 +51,7 @@ class HomePage extends Component {
       {
         key: "title",
         label: "Topic",
-        cell: item => <Link to={`/threads/${item._id}`}>{item.title}</Link>
+        cell: item => <Link to={`/threads/${item._id}`}>{item.title}</Link> // eslint-disable-line no-underscore-dangle
       },
       { key: "name", label: "Name" },
       { key: "timestamp", label: "Date" }
