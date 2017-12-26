ddabc2dacf6ee50fbea237f3acddb8d0bdb87f39
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-12-26 20:56:25 +0100

    Remove unnecessary import

--- src/views/containers/ThreadForm.jsx
+++ src/views/containers/ThreadForm.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { reduxForm } from "redux-form";
 import { connect } from "react-redux";
