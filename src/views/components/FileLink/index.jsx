c2dc3630561dba6e19646378c6a349f6a31b8bfe
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-12-26 20:00:08 +0100

    Fix prettier issue

--- src/views/components/FileLink/index.jsx
+++ src/views/components/FileLink/index.jsx
@@ -7,9 +7,9 @@ const FileLink = styled.a`
   font-family: ${fonts.primary};
   margin-top: 0px;
   font-weight: 800;
-  float:left;
-  margin-right:15px;
-  width:140px;
+  float: left;
+  margin-right: 15px;
+  width: 140px;
 `;
 
 export default FileLink;
