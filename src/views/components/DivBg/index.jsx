bb1bd7759812f0849a7e1a6351dbba2e68c82c6e
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-12-26 19:53:38 +0100

    Fix prettier issue

--- src/views/components/DivBg/index.jsx
+++ src/views/components/DivBg/index.jsx
@@ -2,7 +2,8 @@ import styled from "styled-components";
 import colors from "../../constants/colors";
 
 const DivBg = styled.div`
-  background: ${props => (props.pop ? `${colors.primary}` : `${colors.secondary}`)};
+  background: ${props =>
+    props.pop ? `${colors.primary}` : `${colors.secondary}`};
   padding: 35px;
   border-color: red;
   margin: 20px auto;
