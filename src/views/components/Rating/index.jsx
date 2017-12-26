4598b1ab3d7b2a26692dd924eaf627888996694d
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-12-26 20:28:09 +0100

    Add missing propTypes

--- src/views/components/Rating/index.jsx
+++ src/views/components/Rating/index.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Rating from "react-rating";
 import Container from "./Container";
 
-const RatingWidget = ({ ratingFor, onClick, currentRating, name }) => (
+const propTypes = {
+  currentRating: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
+};
+
+const RatingWidget = ({ onClick, currentRating, name }) => (
   <Container>
     <Rating
       initialRate={currentRating}
@@ -13,4 +20,5 @@ const RatingWidget = ({ ratingFor, onClick, currentRating, name }) => (
   </Container>
 );
 
+RatingWidget.propTypes = propTypes;
 export default RatingWidget;
