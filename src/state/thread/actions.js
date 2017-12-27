ccc770f0fbdaeb35853a3f9643cbc8bbb1ffa0dc
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2017-12-27 06:00:01 +0100

    Fix prettier issues

--- src/state/thread/actions.js
+++ src/state/thread/actions.js
@@ -1,11 +1,21 @@
 import types from "./types";
 import Client from "../../libs/Client";
 
-export const threadHasError = (bool, err) => ({type: types.THREAD_HAS_ERROR, hasError: bool, errorMessage: err});
+export const threadHasError = (bool, err) => ({
+  type: types.THREAD_HAS_ERROR,
+  hasError: bool,
+  errorMessage: err
+});
 
-export const threadIsLoading = bool => ({type: types.THREAD_IS_LOADING, isLoading: bool});
+export const threadIsLoading = bool => ({
+  type: types.THREAD_IS_LOADING,
+  isLoading: bool
+});
 
-export const threadFetchDataSuccess = thread => ({type: types.THREAD_FETCH_DATA_SUCCESS, thread});
+export const threadFetchDataSuccess = thread => ({
+  type: types.THREAD_FETCH_DATA_SUCCESS,
+  thread
+});
 
 export const threadPostDataSuccess = (bool, message) => ({
   type: types.THREAD_POST_DATA_SUCCESS,
@@ -15,8 +25,7 @@ export const threadPostDataSuccess = (bool, message) => ({
 
 export const getThread = url => dispatch => {
   dispatch(threadIsLoading(true));
-  Client
-    .GET(url)
+  Client.GET(url)
     .then(data => {
       dispatch(threadFetchDataSuccess(data.data));
       dispatch(threadIsLoading(false));
@@ -29,8 +38,7 @@ export const getThread = url => dispatch => {
 
 export const postReply = (url1, data, url2) => dispatch => {
   dispatch(threadIsLoading(true));
-  Client
-    .POST(url1, data)
+  Client.POST(url1, data)
     .then(data => {
       if (data.status === 201) {
         dispatch(threadIsLoading(false));
@@ -41,7 +49,7 @@ export const postReply = (url1, data, url2) => dispatch => {
       dispatch(threadHasError(true, err.message));
       dispatch(threadIsLoading(false));
     });
-}
+};
 
 export default {
   threadHasError,
