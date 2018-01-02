1d608f5ea19c039905e27400eba16e34d3f848f7
isabelberg <33488944+isabelberg@users.noreply.github.com>
2018-01-02 17:55:30 +0000

    changes to styling

--- src/views/components/Link/Anchor.jsx
+++ src/views/components/Link/Anchor.jsx
@@ -1,9 +1,17 @@
 import styled from "styled-components";
+import fonts from "../../constants/fonts";
+import colors from "../../constants/colors";
 
 const Anchor = styled.a`
   cursor: pointer;
   line-height:30px;
   text-decoration: none;
+  font-family: ${fonts.primary};
+  color: ${colors.primary};
+  &:hover {
+    text-decoration: underline;
+  }
+  flex-basis: 50%;
 `;
 
 export default Anchor;
