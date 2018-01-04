ab357510ad9a61adde48b7514adfd0ae43d7a656
isabelberg <33488944+isabelberg@users.noreply.github.com>
2018-01-04 16:46:35 +0000

    added styled comp

---
+++ src/views/components/Line/index.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import colors from "../../constants/colors";
+
+const Line = styled.hr`
+  border: none;
+  border-top: solid 1px ${colors.gray};
+  margin-top: 15px;
+`;
+
+export default Line;
